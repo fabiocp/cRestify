aab70d1fdf92a267aba7c3d4f115d21338fbdc67
Marcelo Fernandes <marcelo.fernandes@zimbo.me>
2015-02-24 18:59:18 -0300

    inicial

---
+++ Restify/Restify.cs
@@ -0,0 +1,23 @@
+﻿using Owin;
+using cRestify;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace cRestify {
+
+  public class Restify {
+
+    private IAppBuilder app;
+
+    public Restify(IAppBuilder app) {
+      this.app = app;
+
+    }
+    public Server CreateServer() {
+      return new Server(app);
+    }
+  }
+}
---
+++ Restify/Server.cs
@@ -0,0 +1,67 @@
+﻿using Microsoft.Owin;
+using Owin;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace cRestify {
+
+  public class Server {
+
+    private readonly IAppBuilder app;
+
+    public Server(IAppBuilder app) {
+      this.app = app;
+    }
+
+    public void Post(string path, Func<IOwinRequest, IOwinResponse, Task> handler) {
+      app.Run(context => {
+        if (context.Request.Path.Value == path) {
+          context.Request.ContentType = "text/plain";
+          return handler.Invoke(context.Request, context.Response);
+        }
+        context.Response.StatusCode = 404;
+        return Task.Delay(0);
+      });
+    }
+
+    public void Put(string path, Func<IOwinRequest, IOwinResponse, Task> handler) {
+      app.Run(context => {
+        if (context.Request.Path.Value == path) {
+          context.Request.ContentType = "text/plain";
+          return handler.Invoke(context.Request, context.Response);
+        }
+        context.Response.StatusCode = 404;
+        return Task.Delay(0);
+      });
+    }
+
+    public void Del(string path, Func<IOwinRequest, IOwinResponse, Task> handler) {
+      app.Run(context => {
+        if (context.Request.Path.Value == path) {
+          context.Request.ContentType = "text/plain";
+          return handler.Invoke(context.Request, context.Response);
+        }
+        context.Response.StatusCode = 404;
+        return Task.Delay(0);
+      });
+    }
+
+    public void Get(string path, Func<IOwinRequest, IOwinResponse, Task> handler) {
+      app.Run(context => {
+        if (context.Request.Path.Value == path) {
+          context.Request.ContentType = "text/plain";
+          return handler.Invoke(context.Request, context.Response);
+        }
+        context.Response.StatusCode = 404;
+        return Task.Delay(0);
+      });
+    }
+
+    public void Resource(Object Todo) {
+      throw new NotImplementedException();
+    }
+  }
+}
