f28e46511c51959350a765389382bb150c7e9442
Marcelo Fernandes <marcelo.fernandes@questores.com.br>
2015-03-04 17:36:22 -0300

    teste

--- cRestify/Server.cs
+++ cRestify/Server.cs
@@ -4,8 +4,11 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Linq.Expressions;
+using System.Reflection;
+using System.Runtime.Serialization;
 using System.Text;
 using System.Threading.Tasks;
+using Fasterflect;
 
 namespace cRestify {
 
@@ -18,7 +21,8 @@ namespace cRestify {
 
   public interface ISetupSetter<TMock> where TMock : class {
   }
-
+  public interface ISetup<TMock, TResult> where TMock : class {
+  }
   public class SetterMethodCall<TMock> : ISetupSetter<TMock>
     where TMock : class {
   }
@@ -32,6 +36,12 @@ namespace cRestify {
       return function();
     }
   }
+  internal class CallInfo {
+    public Expression Object { get; set; }
+    public MethodInfo Method { get; set; }
+    public IEnumerable<Expression> Arguments { get; set; }
+  }
+
 
   public class Server {
 
@@ -71,26 +81,111 @@ namespace cRestify {
           });
         }*/
 
-    public void Get<T>( string path, Action<T> handler ) where T : class {
+
+    private static object CreateInstance( Type classType ) {
+      var creator = classType.CreateInstance();
+      return creator;
+    }
+
+    private static Func<object> GetCreator( Type classType ) {
+      Expression body;
+      const BindingFlags bindingFlags = BindingFlags.Public | BindingFlags.NonPublic | BindingFlags.Instance;
+      var defaultConstructor = classType.GetConstructor(bindingFlags, null, new Type[0], null);
+      if (defaultConstructor != null) {
+        body = Expression.New(defaultConstructor);
+      } else {
+        var getUnitializedObjectMethodInfo = typeof(FormatterServices).GetMethod("GetUninitializedObject", BindingFlags.Public | BindingFlags.Static);
+        body = Expression.Call(getUnitializedObjectMethodInfo, Expression.Constant(classType));
+      }
+      var lambdaExpression = Expression.Lambda<Func<object>>(body);
+      return lambdaExpression.Compile();
+
+    }
+    /*
+        public void Get<T>( string path, Action<T> handler ) where T : class {
+          app.Run(context => {
+            //     if (context.Request.Path.Value == path) {
+            context.Request.ContentType = "text/plain";
+            //handler.ToString();
+            var obj =  (T)CreateInstance(typeof(T));
+            handler(obj);
+            var setterExpressionParameters = handler.GetMethodInfo();
+            var res = obj.CallMethod("HelloWord");
+
+            // var methodInfo = obj.UnwrapIfWrapped().GetType().Method("Walk", Flags.InstanceAnyVisibility);
+            //methodInfo.Call(obj);
+            //handler.Invoke(obj);
+            /*return PexProtector.Invoke(() => {
+              return SetupSetImpl<T, SetterMethodCall<T, TProperty>>(mock, setterExpression, ( m, expr, method, value ) => {
+                var call = new SetterMethodCall<T, TProperty>(m, condition, expr, method, value[0]);
+                m.Interceptor.AddCall(call, SetupKind.PropertySet);
+                return call;
+              });
+            });*/
+    //return context.Response.WriteAsync(res);
+    //}
+    /* context.Response.StatusCode = 404;
+     return Task.Delay(0);
+   });
+ }
+*/
+
+    public void Get<T, TResult>( string path, Expression<Func<T, TResult>> expression ) where T : class {
       app.Run(context => {
-        if (context.Request.Path.Value == path) {
-          context.Request.ContentType = "text/plain";
-          handler.ToString();
-          return context.Response.WriteAsync("Hello World");
-        }
+        //     if (context.Request.Path.Value == path) {
+        context.Request.ContentType = "text/plain";
+        //handler.ToString();
+        var obj =  (T)CreateInstance(typeof(T));
+        //handler(obj);
+        // var setterExpressionParameters = handler.GetMethodInfo();
+        var res = obj.CallMethod("HelloWord");
+        var aaa = expression.Name;
+        // var methodInfo = obj.UnwrapIfWrapped().GetType().Method("Walk", Flags.InstanceAnyVisibility);
+        //methodInfo.Call(obj);
+        //handler.Invoke(obj);
+        /*return PexProtector.Invoke(() => {
+          return SetupSetImpl<T, SetterMethodCall<T, TProperty>>(mock, setterExpression, ( m, expr, method, value ) => {
+            var call = new SetterMethodCall<T, TProperty>(m, condition, expr, method, value[0]);
+            m.Interceptor.AddCall(call, SetupKind.PropertySet);
+            return call;
+          });
+        });*/
+        //return context.Response.WriteAsync(res);
+        //}
         context.Response.StatusCode = 404;
         return Task.Delay(0);
       });
-
-
-      /*  return PexProtector.Invoke(() => {
-            return SetupSetImpl<T, SetterMethodCall<T, TProperty>>(mock, setterExpression, ( m, expr, method, value ) => {
-              var call = new SetterMethodCall<T, TProperty>(m, condition, expr, method, value[0]);
-              m.Interceptor.AddCall(call, SetupKind.PropertySet);
-              return call;
-            });
-          });*/
     }
+
+
+    public void Get<T>( string path, Action<T> handler ) where T : class {
+      app.Run(context => {
+        //     if (context.Request.Path.Value == path) {
+        context.Request.ContentType = "text/plain";
+        //handler.ToString();
+        var obj =  (T)CreateInstance(typeof(T));
+        handler(obj);
+        // var setterExpressionParameters = handler.GetMethodInfo();
+        // var res = obj.CallMethod("HelloWord");
+        //var aaa = expression.Name;
+        // var methodInfo = obj.UnwrapIfWrapped().GetType().Method("Walk", Flags.InstanceAnyVisibility);
+        //methodInfo.Call(obj);
+        //handler.Invoke(obj);
+        /*return PexProtector.Invoke(() => {
+          return SetupSetImpl<T, SetterMethodCall<T, TProperty>>(mock, setterExpression, ( m, expr, method, value ) => {
+            var call = new SetterMethodCall<T, TProperty>(m, condition, expr, method, value[0]);
+            m.Interceptor.AddCall(call, SetupKind.PropertySet);
+            return call;
+          });
+        });*/
+        //return context.Response.WriteAsync(res);
+        //}
+        context.Response.StatusCode = 202;
+        return Task.Delay(0);
+      });
+    }
+
+
     /*
         public void head(string path, Func<IOwinRequest, IOwinResponse, Task> handler) {
           app.Run(context => {
--- cRestifyTest/ServerTest.cs
+++ cRestifyTest/ServerTest.cs
@@ -24,6 +24,20 @@ namespace cRestifyTest {
       WebApp.Start(new StartOptions { Port = 8080 }, startup => {
         var restify = new Restify(startup);
         var server = restify.CreateServer();
+        server.Get<Todo, string>("/helloWord", env => env.HelloWord());
+      });
+
+      var client = new HttpClient();
+      client.GetStringAsync("http://localhost:8080/helloWord").Result.ShouldEqual("Hello World");
+
+    }
+
+    [Test]
+    public void TestMethod2() {
+
+      WebApp.Start(new StartOptions { Port = 8080 }, startup => {
+        var restify = new Restify(startup);
+        var server = restify.CreateServer();
         server.Get<Todo>("/helloWord", env => env.HelloWord());
       });
 
@@ -31,5 +45,23 @@ namespace cRestifyTest {
       client.GetStringAsync("http://localhost:8080/helloWord").Result.ShouldEqual("Hello World");
 
     }
+/*
+    [Test]
+    public void TestMethod3() {
+
+      WebApp.Start(new StartOptions { Port = 8080 }, startup => {
+        var restify = new Restify(startup);
+        var server = restify.CreateServer();
+        server.Resource<Todo>("/todo", env => {
+          env.Get("/list");
+          env.Get("/show/:id");
+          env.Get("/new");
+        });
+      });
+
+      var client = new HttpClient();
+      client.GetStringAsync("http://localhost:8080/helloWord").Result.ShouldEqual("Hello World");
+
+    }*/
   }
 }
