9a7be270b1cef298bdc302faf83089045ed38346
Marcelo Fernandes <marcelo.fernandes@questores.com.br>
2015-03-06 11:48:04 -0300

    ex

---
+++ DevStore.Tdc/DevStore.Tdc.Api/Model/Product.cs
@@ -0,0 +1,9 @@
+namespace DevStore.Tdc.Api.Model
+{
+    public partial class Product
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string ProductNumber { get; set; }
+    }
+}
---
+++ DevStore.Tdc/DevStore.Tdc.Web/app/routes.js
@@ -0,0 +1,21 @@
+﻿(function () {
+    'use strict';
+    var id = 'app';
+
+    var app = angular.module('app', [
+        'ngRoute'
+    ]);
+
+    app.config(function ($routeProvider) {
+        $routeProvider
+            .when('/', {
+                controller: 'HomeController as vm',
+                templateUrl: 'app/views/home/index.html'
+               
+            })
+            .when('/produtos', {
+                controller: 'ProductController as vm',
+                templateUrl: 'app/views/product/index.html'
+            });
+    });
+})();
