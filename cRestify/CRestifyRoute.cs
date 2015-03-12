6592de2e171e6a9f346262dde794bf42d126a64d
Fabio Pasquali <fabiocesarpasquali@gmail.com>
2015-03-11 22:50:35 -0300

    *inicio da implementação do método Route()

---
+++ cRestify/CRestifyRoute.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace cRestify
+{
+    public class CRestifyRoute
+    {
+        public string Method{get;set;}
+        public string Path{get;set;}
+        public Func<CRestifyRequest, object> Expr { get; set; }
+
+        public override string ToString()
+        {
+            return Method + ":" + Path;
+        }
+    }
+}
---
+++ cRestify/POSTCRestifyRequest.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.Owin;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace cRestify
+{
+    public class POSTCRestifyRequest : CRestifyRequest
+    {
+        public POSTCRestifyRequest(IOwinRequest owinRequest) : base(owinRequest) { 
+        
+        }
+        public override IDictionary<string, object> GetParams()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
